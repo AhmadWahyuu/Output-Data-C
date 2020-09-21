fb0b247115e647f929221214147771b4e742936e
AhmadWahyuu <wahyu.muhammad.5477@gmail.com>
2020-09-21 14:14:46 +0700

    data pribadi

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main(){
+        printf("nama : Ahmad Wahyu Rafsan Zani/n");
+	printf("Npm  : 19081010119/n");
+	printf("Fakultas : Ilmu Komputer/n");
+	printf("Jurusan  : Informatika/n");
+}
